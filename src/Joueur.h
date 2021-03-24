7c39ba043adb49942a28083b5dd8331a24d8e873
Ildès Rampon <ipoitou@ecomail.fr>
2021-03-24 17:17:34 +0100

    création Joueur.cpp et MàJ Joueur.h

--- src/Joueur.h
+++ src/Joueur.h
@@ -1,28 +1,59 @@
+#ifndef JOUEUR_H
+#define JOUEUR_H
 
+#include <string>
+#include <vector>
+#include "Carte.h"
+
+using namespace std;
 
 class Joueur
 {
 private:
-		string nom;
-		Carte * main; //vector
 
 	 
 
 public:
+		string nom;
+		vector<Carte> main;		
+		unsigned int numeroJoueur;
+
+		
 		/**
-		* @brief Construit une nouvelle carte.
+		* @brief 
+		* Constructeur par défaut. Initialise numeroJoueur à 1 et nom à Joueur 1.
 		* 
 		*/
 		Joueur();
-
 		
+		/**
+		* @brief 
+		* Constructeur. Affecte num à numeroJoueur et n à nom.
+		* 
+		* @param
+		* [in] entier naturel inférieur ou égal au nombre de joueurs
+		* [in] chaîne de caractères
+		*/
+		Joueur (const unsigned int num, const string n);
+		
+		/**
+		* @brief 
+		* Destructeur.
+		* 
+		*/
 		~Joueur();
 
 
-		void gagnant();
+		/**
+		* @brief 
+		* Détermine si le joueur est gagnant.
+		* 
+		* @return
+		* Vrai si la main du joueur est vide
+		*/
+		bool gagnant();
 
 
-		void afficherMainJoueur();
 
 		
 				
