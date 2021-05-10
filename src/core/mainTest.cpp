92144e985933f44b5a8b08b7e8274d4d2f38134a
Ildès Rampon <ipoitou@ecomail.fr>
2021-05-10 11:31:33 +0200

    dossiers dans src

---
+++ src/core/mainTest.cpp
@@ -0,0 +1,16 @@
+#include "AffichageTxt.h"
+#include "Fenetre.h"
+#include <SalleAttente.h>
+#include <iostream>
+
+using namespace std;
+
+int main(int argc, char **argv)
+{
+
+    Jeu jeu(2,1);
+    jeu.testRegression();
+    jeu.joueursBot[0].testRegression(jeu);
+    
+    return 0;
+}
